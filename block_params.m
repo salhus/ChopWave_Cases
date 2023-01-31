ca2100b67882f24c6c5dbef723fe081abd853714
Husain <shusain@nrel.gov>
2023-01-31 12:19:27 -0700

    make_examples_work

--- block_params.m
+++
@@ -1,7 +0,0 @@
-load_system('OSWEC')
-BlockPaths = find_system('OSWEC/Global Reference Frame/waveVis/waveVisOn/water elevation/marker1/Visualization Block','Type','Block');
-handle = getSimulinkBlockHandle('OSWEC/Global Reference Frame/waveVis/waveVisOn/water elevation/marker1/Visualization Block/WaveElevation');
-ObjectParameters = get_param(handle,'ObjectParameters');
-DialogParameters = get_param(handle,'DialogParameters');
-p = Simulink.Mask.get(handle);
-MaskVars = p.getWorkspaceVariables;
